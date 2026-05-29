21751179f88695ea34cd98d4080a187dc3cec8ad
nymera <frostiesnowman439@gmail.com>
2026-05-29 14:31:48 +0800

    Update deadline_silentaim.lua

--- games/deadline_silentaim.lua
+++ games/deadline_silentaim.lua
@@ -1,122 +1,129 @@
 -- much luv w/ dementiaenjoyer <3
-local config = {
-    enabled = true,
-    hit_chance = 100,
-    max_distance = 300,
-    bullet_offset = Vector3.new(0, 0, 0),
+local Config = {
+    Enabled = true,
+    HitChance = 100,
+    MaxDistance = 300,
+    BulletOffset = Vector3.new(0, 0, 0),
     
-    target_part = "head",
-    character_folder = "characters",
+    TargetPart = "head",
+    CharacterFolder = "characters",
 
-    use_custom_speed = true,
-    bullet_speed = 1000,
+    UseCustomSpeed = true,
+    BulletSpeed = 1000,
     
-    use_multishot = true,
-    bullet_amount = 3,
+    UseMultishot = true,
+    BulletAmount = 3,
 
-    on_shoot = function(target_player, self_param, data, origin, direction, ...)
+    OnShoot = function(TargetPlayer, SelfParam, Data, Origin, Direction, ...)
         
     end
 }
 
-local replicated_storage = game:GetService("ReplicatedStorage")
-local camera = workspace.CurrentCamera
-local modules = {}
-local old_fire_server = nil
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Camera = workspace.CurrentCamera
+local Modules = {}
+local OldFireServer = nil
 
-local function get_closest_player()
-    local closest_distance = config.max_distance
-    local closest_player = nil
-    local char_folder = workspace:FindFirstChild(config.character_folder)
+local function GetClosestPlayer()
+    local ClosestDistance = Config.MaxDistance
+    local ClosestPlayer = nil
+    local CharFolder = workspace:FindFirstChild(Config.CharacterFolder)
     
-    if not char_folder then return nil end
+    if not CharFolder then
+        return nil
+    end
 
-    for _, player in char_folder:GetChildren() do
-        if not player:FindFirstChild("hitbox") or not player:FindFirstChild(config.target_part) then
+    for _, Player in CharFolder:GetChildren() do
+        if not Player:FindFirstChild("hitbox") or not Player:FindFirstChild(Config.TargetPart) then
             continue
         end
         
-        local center = Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 2)
-        local position, on_screen = camera:WorldToViewportPoint(player.hitbox.Position)
+        local Center = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
+        local Position, OnScreen = Camera:WorldToViewportPoint(Player.hitbox.Position)
         
-        if not on_screen then
+        if not OnScreen then
             continue
         end
         
-        local distance = (center - Vector2.new(position.X, position.Y)).Magnitude
-        if distance > closest_distance then
+        local Distance = (Center - Vector2.new(Position.X, Position.Y)).Magnitude
+        
+        if Distance > ClosestDistance then
             continue
         end
         
-        closest_distance = distance
-        closest_player = player
+        ClosestDistance = Distance
+        ClosestPlayer = Player
     end
     
-    return closest_player
+    return ClosestPlayer
 end
 
-for _, module in replicated_storage.module:GetDescendants() do
-    if string.find(module.ClassName, "Script") then
-        modules[module.Name] = module
+for _, Module in ReplicatedStorage.module:GetDescendants() do
+    if string.find(Module.ClassName, "Script") then
+        Modules[Module.Name] = Module
     end
 end
 
-local caster = require(modules.caster)
+local Caster = require(Modules.caster)
 
-for _, upvalue in debug.getupvalues(caster.fire) do
-    if typeof(upvalue) == "function" then
-        local constants = debug.getconstants(upvalue)
-        if constants[1] == "pcall" and constants[9] == "kill yourself" then
-            hookfunction(upvalue, function()
+for _, Upvalue in debug.getupvalues(Caster.fire) do
+    if typeof(Upvalue) == "function" then
+        local Constants = debug.getconstants(Upvalue)
+        
+        if Constants[1] == "pcall" and Constants[9] == "kill yourself" then
+            hookfunction(Upvalue, function()
                 return
             end)
         end
     end
 end
 
-old_fire_server = hookfunction(caster.fire, function(self_param, origin_pos, direction, data, ...)
-    local closest_player = nil
+OldFireServer = hookfunction(Caster.fire, function(SelfParam, OriginPos, Direction, Data, ...)
+    local ClosestPlayer = nil
     
-    if config.enabled then
-        local roll = math.random(1, 100)
+    if Config.Enabled then
+        local Roll = math.random(1, 100)
         
-        if roll <= config.hit_chance then
-            closest_player = get_closest_player()
-            if closest_player then
-                origin_pos += config.bullet_offset
-                local target_pos = closest_player[config.target_part].Position
+        if Roll <= Config.HitChance then
+            ClosestPlayer = GetClosestPlayer()
+            
+            if ClosestPlayer then
+                OriginPos += Config.BulletOffset
                 
-                local speed = config.use_custom_speed and config.bullet_speed or direction.Magnitude
-                direction = (target_pos - origin_pos).Unit * speed
+                local TargetPos = ClosestPlayer[Config.TargetPart].Position
+                local Speed = Config.UseCustomSpeed and Config.BulletSpeed or Direction.Magnitude
+                
+                Direction = (TargetPos - OriginPos).Unit * Speed
                 
                 -- Update internal data table velocity if it exists
-                if type(data) == "table" and data.Velocity then
-                    data.Velocity = direction
+                if type(Data) == "table" and Data.Velocity then
+                    Data.Velocity = Direction
                 end
             end
-        elseif config.use_custom_speed then
-            direction = direction.Unit * config.bullet_speed
-            if type(data) == "table" and data.Velocity then
-                data.Velocity = direction
+        elseif Config.UseCustomSpeed then
+            Direction = Direction.Unit * Config.BulletSpeed
+            
+            if type(Data) == "table" and Data.Velocity then
+                Data.Velocity = Direction
             end
         end
     else
-        if config.use_custom_speed then
-            direction = direction.Unit * config.bullet_speed
+        if Config.UseCustomSpeed then
+            Direction = Direction.Unit * Config.BulletSpeed
         end
     end
     
-    if typeof(config.on_shoot) == "function" then
-        task.spawn(config.on_shoot, closest_player, self_param, data, origin_pos, direction, ...)
+    if typeof(Config.OnShoot) == "function" then
+        task.spawn(Config.OnShoot, ClosestPlayer, SelfParam, Data, OriginPos, Direction, ...)
     end
     
-    if config.use_multishot and config.bullet_amount > 1 then
-        for i = 1, (config.bullet_amount - 1) do
-            old_fire_server(self_param, origin_pos, direction, data, ...)
+    if Config.UseMultishot and Config.BulletAmount > 1 then
+        for i = 1, (Config.BulletAmount - 1) do
+            OldFireServer(SelfParam, OriginPos, Direction, Data, ...)
         end
     end
     
-    return old_fire_server(self_param, origin_pos, direction, data, ...)
+    return OldFireServer(SelfParam, OriginPos, Direction, Data, ...)
 end)
 
-return config
+return Config
